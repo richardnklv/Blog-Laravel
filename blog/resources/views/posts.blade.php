cb12bc9c973bc9381ac007b6cbe7a67edab7cfa8
Richard Nikolov <richardnklv@outlook.com>
2024-02-16 09:34:05 +0100

    .

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -4,29 +4,21 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
+    <?php foreach ($posts as $post) : ?>
     <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <h1>
+                <a href="/posts/<?= $post->slug; ?>">
+                <?= $post->title; ?>
+                </a>
+        </h1>
 
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi assumenda consequuntur distinctio enim illo, ipsa labore laboriosam laborum nesciunt obcaecati omnis pariatur praesentium quae quo saepe sit tempora voluptates voluptatibus.
-        </p>
+
+        <div>
+            <?= $post->excerpt; ?>
+        </div>
+
     </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi assumenda consequuntur distinctio enim illo, ipsa labore laboriosam laborum nesciunt obcaecati omnis pariatur praesentium quae quo saepe sit tempora voluptates voluptatibus.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi assumenda consequuntur distinctio enim illo, ipsa labore laboriosam laborum nesciunt obcaecati omnis pariatur praesentium quae quo saepe sit tempora voluptates voluptatibus.
-        </p>
-    </article>
+    <?php endforeach; ?>
 </body>
 
 
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,22 +17,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    $files = File::files(resource_path("posts/"));
+
+    $posts = array_map(function ($file) {
+        $document = YamlFrontMatter::parseFile($file);
+
+        return new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug
+        );
+    }, $files);
+
+
+    return view('posts', [
+        'posts' => $posts
+    ]);
 });
 
 Route::get("posts/{post}", function ($slug) {
+    // Find a post by its slug and pass it to a view called "post"
 
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if (! file_exists($path)) {
-        return redirect('/');
-    }
-
-    $post = cache()->remember("posts.{$slug}", 5, function () use ($path) {
-        return file_get_contents($path);
-    });
-
-
-    return view('post', ['post' => $post ]);
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
 })->where('post', '[A-z_\-]+');
 
