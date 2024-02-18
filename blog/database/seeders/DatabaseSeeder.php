b7cf8d8e680250d8394c6c927389bff130123b94
Richard Nikolov <richardnklv@outlook.com>
2024-02-18 14:46:22 +0100

    seeders

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,19 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         $user = User::factory(3)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
 
-         Category::create([
+         $user = User::factory()->create();
+
+         $personal = Category::create([
             'name' => 'Personal',
              'slug' => 'personal'
          ]);
 
-        Category::create([
+        $family = Category::create([
             'name' => 'Family',
             'slug' => 'family'
         ]);
 
-        Category::create([
+        $work = Category::create([
             'name' => 'Work',
             'slug' => 'work'
         ]);
@@ -34,6 +39,24 @@ class DatabaseSeeder extends Seeder
          User::factory()->create([
              'name' => 'Test User',
              'email' => 'test@example.com',
+         ]);
+
+         Post::create([
+             'user_id' => $user->id,
+             'category_id' => $family->id,
+             'title' => 'My Family Post',
+             'slug' => 'my-first-post',
+             'excerpt' => 'Lorem ipsum dolar sit amet.',
+             'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus dolor gravida posuere luctus. Etiam egestas accumsan neque. Proin eu augue massa. Nam quis vestibulum lectus. Proin placerat lectus nec magna interdum, in rhoncus arcu accumsan. Vestibulum eget est erat. Proin dictum ante a fermentum tristique. Ut luctus mi sapien, et interdum ipsum maximus eget. Maecenas ex lectus, mattis nec nisi vel, hendrerit tristique neque.'
+         ]);
+
+         Post::create([
+             'user_id' => $user->id,
+             'category_id' => $work->id,
+             'title' => 'My Work Post',
+             'slug' => 'my-second-post',
+             'excerpt' => 'Lorem ipsum dolar sit amet.',
+             'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus dolor gravida posuere luctus. Etiam egestas accumsan neque. Proin eu augue massa. Nam quis vestibulum lectus. Proin placerat lectus nec magna interdum, in rhoncus arcu accumsan. Vestibulum eget est erat. Proin dictum ante a fermentum tristique. Ut luctus mi sapien, et interdum ipsum maximus eget. Maecenas ex lectus, mattis nec nisi vel, hendrerit tristique neque.'
          ]);
     }
 }
